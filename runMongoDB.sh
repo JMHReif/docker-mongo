16d3e80b4c7830116541e3cefeaa2c8e0f4eab68
Jennifer Reif <jennifer.reif@neo4j.com>
2021-08-16 12:48:27 -0500

    Update to latest image, add readme

--- runMongoDB.sh
+++ runMongoDB.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-docker run --name mymongo -p27017:27017 -d -v $HOME/Projects/docker/data/mongo:/data/db -v $HOME/Projects/docker/logs/mongo:/logs jmreif/mongodb
+docker run --name mymongo -p27017:27017 \
+    -e MONGO_INITDB_ROOT_USERNAME=mongoadmin \
+    -e MONGO_INITDB_ROOT_PASSWORD=Testing123 \
+    -d -v $HOME/Projects/docker/data/mongo:/data/db \
+    -v $HOME/Projects/docker/logs/mongo:/logs \
+    -v $HOME/Projects/docker/tmp/mongo:/tmp \
+    jmreif/mongodb
 #TEST docker run --name mymongo -p27017:27017 -v $HOME/Projects/docker/data/mongo:/data/db -v $HOME/Projects/docker/logs/mongo:/logs jmreif/mongodb
 docker ps
